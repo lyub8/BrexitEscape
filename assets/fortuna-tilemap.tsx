5b69703fb5a4760d2e9dd73f390fc82df6e570bd
Lyubomir Ivanov <kwb15150@uni.strath.ac.uk>
2018-10-13 08:58:39 +0100

    first map created

---
+++ assets/fortuna-tilemap.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="fortuna-tilemap" tilewidth="16" tileheight="16" tilecount="7500" columns="75">
+ <image source="fortuna-tilemap.png" width="1200" height="1600"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="-0.181818">
+    <polygon points="0,0 -0.181818,16.3636 15.8182,16 16,0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="13.3333" y="1">
+    <polygon points="0,0 0,14 7.33333,15 10.3333,0.333333 8.66667,-7"/>
+   </object>
+   <object id="2" x="11.3333" y="0.666667" width="11.3333" height="14.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="680">
+  <objectgroup draworder="index">
+   <object id="1" x="0.272727" y="0.0909091" width="9.90909" height="15.6364"/>
+  </objectgroup>
+ </tile>
+</tileset>
